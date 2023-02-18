578f66ee8a80d887844e064d9da35f6823a3f131
obiomaokorowu <austinobi983@gmail.com>
2023-02-18 14:43:57 +0100

    automate installations

---
+++ jenkins_install.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+sudo apt update
+sudo apt install openjdk-11-jdk -y
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 62A9756BFD780C377CF24BA8FCEF32E745F2C3D5
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt install ca-certificates
+sudo apt update
+sudo apt install git -y
+sudo apt install maven -y
+sudo apt install ansible -y
+sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt update
+sudo apt install jenkins -y
+sudo apt-get update 
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+    gpg --dearmor | \
+    sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+gpg --no-default-keyring \
+    --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
+    --fingerprint
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+    https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+    sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update
+sudo apt install terraform -y
+sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-key fingerprint 0EBFCD88
+sudo apt-get update 
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+sudo usermod -aG docker jenkins
+sudo usermod -aG docker ubuntu
+sudo usermod -aG docker $USER
+
+sudo apt install awscli -y
+wget https://get.helm.sh/helm-v3.2.4-linux-amd64.tar.gz
+tar -zxvf helm-v3.2.4-linux-amd64.tar.gz
+sudo mv linux-amd64/helm /usr/local/bin/helm
