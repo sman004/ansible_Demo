c0048341c6ab95ed2ea786eb9552adf3c5fffa84
obiomaokorowu <austinobi983@gmail.com>
2023-04-23 15:32:30 +0100

    .\main.tf

--- main.tf
+++ main.tf
@@ -81,6 +81,14 @@ resource "aws_security_group" "ec2_security_group" {
     cidr_blocks      = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description      = "mysql access"
+    from_port        = 3306
+    to_port          = 3306
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port        = 0
     to_port          = 0
